879dfa993871561673160b7c659d2754e94c091c
deeps <deepshikha@qlicket.com>
2019-12-23 12:25:42 +0530

    bookmark

--- app/src/main/java/com/example/lottie/MainActivity.java
+++ app/src/main/java/com/example/lottie/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.lottie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    LottieAnimationView bookark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bookark = findViewById(R.id.book);
     }
+
+    public void bookmarkClick(View view) {
+        bookark.setProgress(0);
+        bookark.pauseAnimation();
+        bookark.playAnimation();
+            }
 }
